f03c84943adf4c0addc5f4ba8567fb8da8efcde6
Derito <helderdacsta@hotmail.com>
2022-01-29 00:08:00 +0100

    Adition of package and class Repository and Service

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -2,8 +2,10 @@ package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication //(scanBasePackages = "one.digitalinnovation.personapi")
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
